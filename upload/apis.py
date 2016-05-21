b9469997f23f4a71b7a97a94a776258f5f0d1e64
joway <joway.w@gmail.com>
2016-05-21 23:16:42 +0800

    add data api

--- upload/apis.py
+++ upload/apis.py
@@ -3,18 +3,21 @@ import logging
 from qiniu import Auth
 from rest_framework import viewsets
 from rest_framework.decorators import list_route
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from config import settings
+from upload.models import File
 
 logger = logging.getLogger(__name__)
 
 access_key = settings.QINIU_ACCESS_KEY
 secret_key = settings.QINIU_SECRET_KEY
 bucket_name = 'i2pserver'
+base_url = 'o7f53wsde.bkt.clouddn.com'
+
 policy = {
-    'callbackUrl': 'http://joway.tunnel.phpor.me/upload/callback/',
+    'callbackUrl': 'http://api.joway.wang/upload/callback/',
     'callbackBody': 'filename=$(fname)&filesize=$(fsize)&type=$(mimeType)&hash=$(etag)'
 }
 q = Auth(access_key, secret_key)
@@ -23,8 +26,9 @@ q = Auth(access_key, secret_key)
 # 七牛云存储支持
 class UploadViewSet(viewsets.GenericViewSet):
     permission_classes = [AllowAny]
+    queryset = File.objects.all()
 
-    @list_route(methods=['get'])
+    @list_route(methods=['post'])
     def token(self, request, *args, **kwargs):
         """
         Get open problem detail
@@ -44,4 +48,10 @@ class UploadViewSet(viewsets.GenericViewSet):
             'hash': request.POST.get('hash'),
         }
         logger.info(info)
+        File.objects.create(url=base_url + info['filename'],
+                            mime_type=info['type'], hash=info['hash'])
         return Response({"message": "callback success"})
+
+    @list_route(methods=['get'])
+    def data(self, request, *args, **kwargs):
+        return Response(self.get_queryset())
